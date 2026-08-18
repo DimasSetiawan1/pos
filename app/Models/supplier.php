f4ec90e31b7c2495e786c65d7e1c11413a5cb30a
DimasSetiawan1 <mdimassetiawan.xia9@gmail.com>
2026-08-19 02:55:10 +0700

    rename Supplier

--- app/Models/supplier.php
+++ app/Models/supplier.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Supplier extends Model
+class Supplier extends Model 
 {
     use HasFactory;
 
